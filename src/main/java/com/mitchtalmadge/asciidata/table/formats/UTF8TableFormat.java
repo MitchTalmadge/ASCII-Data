971bba2e4944d1d5921c72e1273a2dfb0a23cb58
Mitch Talmadge <mitch@mitchtalmadge.com>
2017-10-01 21:50:46 -0600

    Added TableFormatAbstract and UTF8TableFormat.

---
+++ src/main/java/com/mitchtalmadge/asciidata/table/formats/UTF8TableFormat.java
@@ -0,0 +1,81 @@
+package com.mitchtalmadge.asciidata.table.formats;
+
+/**
+ * The default, UTF-8 based table format for {@link com.mitchtalmadge.asciidata.table.ASCIITable}.
+ * Uses double pipes in most cases.
+ */
+public class UTF8TableFormat extends TableFormatAbstract {
+
+    @Override
+    public char getTopLeftCorner() {
+        return '╔';
+    }
+
+    @Override
+    public char getTopRightCorner() {
+        return '╗';
+    }
+
+    @Override
+    public char getBottomLeftCorner() {
+        return '╚';
+    }
+
+    @Override
+    public char getBottomRightCorner() {
+        return '╝';
+    }
+
+    @Override
+    public char getTopEdgeBorderDivider() {
+        return '╤';
+    }
+
+    @Override
+    public char getBottomEdgeBorderDivider() {
+        return '╧';
+    }
+
+    @Override
+    public char getLeftEdgeBorderDivider(boolean underHeaders) {
+        if (underHeaders)
+            return '╠';
+        else
+            return '╟';
+    }
+
+    @Override
+    public char getRightEdgeBorderDivider(boolean underHeaders) {
+        if (underHeaders)
+            return '╣';
+        else
+            return '╢';
+    }
+
+    @Override
+    public char getHorizontalBorderFill(boolean edge, boolean underHeaders) {
+        if (edge || underHeaders)
+            return '═';
+        else
+            return '─';
+    }
+
+    @Override
+    public char getVerticalBorderFill(boolean edge) {
+        if (edge)
+            return '║';
+        else
+            return '│';
+    }
+
+    @Override
+    public char getCross(boolean underHeaders, boolean emptyData) {
+        if (underHeaders) {
+            if (emptyData)
+                return '╧';
+            else
+                return '╪';
+        } else
+            return '┼';
+    }
+}
